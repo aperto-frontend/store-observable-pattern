2faf873602235638476e1d56f951506907947939
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2017-09-11 23:43:30 +0200

    Update packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,12 @@ import $ from 'jquery';
 import Veams from 'veams';
 import Handlebars from 'handlebars/runtime';
 
-import VeamsLogger from 'veams/src/js/plugins/logger';
-import VeamsDOM from 'veams/src/js/plugins/dom';
-import VeamsVent from 'veams/src/js/plugins/vent';
-import VeamsModules from 'veams/src/js/plugins/modules';
-import VeamsTemplater from 'veams/src/js/plugins/templater';
-import VeamsMediaQueryHandler from 'veams/src/js/plugins/media-query-handler';
+import VeamsLogger from 'veams-plugin-logger';
+import VeamsDOM from 'veams-plugin-dom';
+import VeamsVent from 'veams-plugin-vent';
+import VeamsModules from 'veams-plugin-modules';
+import VeamsTemplater from 'veams-plugin-templater';
+import VeamsMediaQueryHandler from 'veams-plugin-media-query-handler';
 import EVENTS from './events';
 import Templates from './templates/templates';
 
@@ -23,9 +23,6 @@ App.version = "0.0.1";
 
 // Veams
 Veams.onInitialize(() => {
-
-	store.initialize();
-
 	/**
 	* Veams Plugins
 	*/
